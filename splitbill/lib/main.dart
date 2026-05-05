2a6dc84cfc421b6d3229778d45034963159c048b
Rodrigo <rodrigoetomas22@gmail.com>
2026-05-05 12:22:41 +0100

    publicação da branch

--- splitbill/lib/main.dart
+++ splitbill/lib/main.dart
@@ -1,5 +1,5 @@
 import 'package:flutter/material.dart';
-import 'telas/tela1.dart';
+import 'screens/tela1.dart';
 
 void main() {
   runApp(const MyApp());
@@ -10,9 +10,9 @@ class MyApp extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return const MaterialApp(
+    return MaterialApp(
       debugShowCheckedModeBanner: false,
-      home: Tela1(),
+      home: const Tela1(),
     );
   }
 }
